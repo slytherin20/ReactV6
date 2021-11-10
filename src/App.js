85760e00d64f829b03963e05c10c075ceb7fb426
slytherin20 <sonaligupta2904@gmail.com>
2021-11-10 16:22:16 +0530

    No frills react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+const Pet = (props) => (
+    React.createElement("div",{},
+    [
+        React.createElement("h2",{},props.name),
+        React.createElement("h3",{},props.animal),
+        React.createElement("h3",{},props.breed),
+    ]
+    )
+)
+
+
 const App = () =>(
     React.createElement(
        "div",
        {},
-       React.createElement("h1",{id:"logo"},"Adopt Me!")
+       React.createElement("h1",{id:"logo"},"Adopt Me!"),
+       React.createElement(Pet,{name:"Luna",animal:"Dog",breed:"Havanese"}),
+       React.createElement(Pet,{name:"Brody",animal:"Cat",breed:"Siemen"}),
+       React.createElement(Pet,{name:"Meridith",animal:"Cat",breed:"Bengal Cat"}),
+
    )
 )
 ReactDOM.render(React.createElement(App),
