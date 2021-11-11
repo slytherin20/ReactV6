51a4cd1a4c5d354d04af327d01c99d93618a997d
slytherin20 <sonaligupta2904@gmail.com>
2021-11-11 10:31:25 +0530

    Added JS tools section code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-const Pet = (props) => (
-    React.createElement("div",{},
-    [
-        React.createElement("h2",{},props.name),
-        React.createElement("h3",{},props.animal),
-        React.createElement("h3",{},props.breed),
-    ]
-    )
-)
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
+const App = () =>
+  React.createElement(
+    "div",
+    {},
+    React.createElement("h1", { id: "logo" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Brody",
+      animal: "Cat",
+      breed: "Siemen",
+    }),
+    React.createElement(Pet, {
+      name: "Meridith",
+      animal: "Cat",
+      breed: "Bengal Cat",
+    })
+  );
 
-const App = () =>(
-    React.createElement(
-       "div",
-       {},
-       React.createElement("h1",{id:"logo"},"Adopt Me!"),
-       React.createElement(Pet,{name:"Luna",animal:"Dog",breed:"Havanese"}),
-       React.createElement(Pet,{name:"Brody",animal:"Cat",breed:"Siemen"}),
-       React.createElement(Pet,{name:"Meridith",animal:"Cat",breed:"Bengal Cat"}),
-
-   )
-)
-ReactDOM.render(React.createElement(App),
-document.getElementById("root")
-)
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
